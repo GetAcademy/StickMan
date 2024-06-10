53eb3e6bebd095b027706c5d32ea9b703acc23ab
GET Academy <terje@kolderup.net>
2024-06-10 12:53:18 +0200

    Initial commit

---
+++ StickMan/Program.cs
@@ -0,0 +1,15 @@
+﻿using StickManApp;
+
+var stickManA = new StickMan(3, 3);
+var stickManB = new StickMan(10, 10);
+stickManA.Draw();
+stickManB.Draw();
+
+StickMan.Dummy(true, "fsdjk");
+
+/*
+ O/
+/|
+/ \
+*/   
+   
---
+++ StickMan/StickMan.cs
@@ -0,0 +1,22 @@
+﻿namespace StickManApp
+{
+    internal class StickMan
+    {
+        public StickMan(int col, int row)
+        {
+            
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(10, 10);
+            Console.WriteLine(" O/");
+            Console.WriteLine("/| ");
+            Console.WriteLine("/ \\");
+        }
+
+        public static void Dummy(bool b, string s)
+        {
+        }
+    }
+}
